46574d41f5c37b625fd6be381be2d27e1d8c78f7
kevinlynx <zmhn320@163.com>
2011-12-14 15:35:26 +0800

    delete dep.lisp

--- dep.lisp
+++
@@ -1,9 +0,0 @@
-(ql:quickload 'restas)
-(ql:quickload 'cl-store)
-(ql:quickload 'closure-template)
-(ql:quickload 'local-time)
-(ql:quickload 's-xml-rpc)
-;(ql:quickload 'restas.file-publisher)
-;(ql:quickload 'image)
-;(ql:quickload 'kl-verify)
-
